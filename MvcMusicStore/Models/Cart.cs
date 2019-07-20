348ee325fa07ba350a6382c892de2dca08547d81
dnkato <diane.n.kato@gmail.com>
2019-07-20 16:41:45 -0700

    Part 8: Shopping Cart with Ajax updates

---
+++ MvcMusicStore/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int RecordId { get; set; }
+        public string CartId { get; set; }
+        public int AlbumId { get; set; }
+        public int Count { get; set; }
+        public System.DateTime DateCreated { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
