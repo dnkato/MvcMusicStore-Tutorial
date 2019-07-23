977ab6b039f7417ffaf89fbe9a7ad2c1b09211d2
dnkato <diane.n.kato@gmail.com>
2019-07-22 18:36:37 -0700

    Move javascript into a separate file to conform with the best practice of "Unobtrusive Javascript" -- i.e. keeping Javascript separate from the markup.

---
+++ MvcMusicStore/Scripts/App/RemoveFromCart.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    // Document.ready -> link up remove event handler
+    $(".RemoveLink").click(function () {
+        // Get the id from the link
+        var recordToDelete = $(this).attr("data-id");
+        if (recordToDelete != '') {
+            // Perform the ajax post
+            $.post("/ShoppingCart/RemoveFromCart", { "id": recordToDelete },
+                function (data) {
+                    // Successful requests get here
+                    // Update the page elements
+                    if (data.ItemCount == 0) {
+                        $('#row-' + data.DeleteId).fadeOut('slow');
+                    } else {
+                        $('#item-count-' + data.DeleteId).text(data.ItemCount);
+                    }
+                    $('#cart-total').text(data.CartTotal);
+                    $('#update-message').text(data.Message);
+                    $('#cart-status').text('Cart (' + data.CartCount + ')');
+                });
+        }
+    });
+});
