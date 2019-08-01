bc6261ea8ce11a4e1a5f8a655e1487f70bcf0869
dnkato <diane.n.kato@gmail.com>
2019-08-01 15:56:45 -0700

    Fix errors occurring when cloning repo

--- MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -8,6 +8,12 @@ namespace MvcMusicStore.Models
 {
     public class MusicStoreEntities : DbContext
     {
+
+        // Specify the name of the connections string which will be added in web.config
+        public MusicStoreEntities() : base("MusicStoreEntities")
+        {
+        }
+
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Genre> Genres { get; set; }
